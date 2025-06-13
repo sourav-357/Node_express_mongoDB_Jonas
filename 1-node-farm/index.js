fa7bc9b87d93f4ee95a4716800d811a6a94adceb
Sourav Kumar <xsouravkumar357@gmail.com>
2025-06-13 13:36:16 +0530

    Read and write the files

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,4 +1,10 @@
 const fs = require('fs');
 
-const hello = 'Hello world';
-console.log(hello);
+const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
+console.log(textIn);
+
+const textOut = `This is what we know about avacado: ${textIn}\nCreated on ${Date.now()}`;
+console.log(textOut);
+
+fs.writeFileSync('./txt/output.txt', 'textOut');
+console.log(`File written!`);

025d44af78358c045e7f7f80b4c93751baf4f213
Sourav Kumar <xsouravkumar357@gmail.com>
2025-06-13 15:37:51 +0530

    Reading and writting files Asynchronically

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,5 +1,8 @@
 const fs = require('fs');
 
+
+// ------------------------ Reading and writting files Synchronically------------------------//
+
 // Reading content from the file
 const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
 console.log(textIn);
@@ -12,3 +15,28 @@ console.log(textOut);
 fs.writeFileSync('./txt/output.txt', textOut);
 console.log(`File written!`);
 
+
+// ------------------------ Reading and writting files Asynchronically ------------------------//
+
+fs.readFile('./txt/start.txt', 'utf-8', (err, data) => {
+     console.log(data);
+});
+console.log("will read file!");
+
+
+// reading files asynchronically by callback hell method
+fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+     fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+          console.log(data2);
+          fs.readFile(`./txt/append.txt`, 'utf-8', (err, data3) => {
+               console.log(data3);
+
+               // writting the file asynchronously 
+               fs.writeFile(`./txt/final.txt`, `${data2}\n${data3}`, 'utf-8', (err) => {
+                    console.log(`Your file has been written 😊`);
+               })
+          });
+     });
+});
+console.log("will read file!");
+console.log(fs.readFileSync(`./txt/final.txt`, 'utf-8'));

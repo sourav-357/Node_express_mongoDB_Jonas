8fca83838fce365258efb8e64dd466f7b22b3bb4
Sourav Kumar <xsouravkumar357@gmail.com>
2025-06-13 13:42:08 +0530

    Updated the comments

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,10 +1,14 @@
 const fs = require('fs');
 
+// Reading content from the file
 const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
 console.log(textIn);
 
+// Definign what to write in the file
 const textOut = `This is what we know about avacado: ${textIn}\nCreated on ${Date.now()}`;
 console.log(textOut);
 
-fs.writeFileSync('./txt/output.txt', 'textOut');
+// Writting a file 
+fs.writeFileSync('./txt/output.txt', textOut);
 console.log(`File written!`);
+

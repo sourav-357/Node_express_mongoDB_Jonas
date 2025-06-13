c9af593cb09778e3b18b3693aa1a289e11f1ebb7
Sourav Kumar <xsouravkumar357@gmail.com>
2025-06-14 01:14:25 +0530

    Project Successful

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -3,9 +3,10 @@ const http = require('http');
 const url = require('url');
 
 
-/*
-// ------------------------ Reading and writting files Synchronically------------------------//
 
+// ----------------------------------- Reading and writting files Synchronically -----------------------------------//
+
+/*
 // Reading content from the file
 const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
 console.log(textIn);
@@ -19,13 +20,13 @@ fs.writeFileSync('./txt/output.txt', textOut);
 console.log(`File written!`);
 
 
-// ------------------------ Reading and writting files Asynchronically ------------------------//
+// ----------------------------------- Reading and writting files Asynchronically -----------------------------------//
+
 
 fs.readFile('./txt/start.txt', 'utf-8', (err, data) => {
      console.log(data);
 });
 console.log("will read file!");
-
 
 // reading files asynchronically by callback hell method
 fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
@@ -45,7 +46,7 @@ console.log("will read file!");
 console.log(fs.readFileSync(`./txt/final.txt`, 'utf-8'));
 */
 
-// ------------------------ Routing ------------------------//
+// ----------------------------------- Routing -----------------------------------//
 
 /*
 // Creating a server
@@ -74,7 +75,7 @@ server.listen(8000, '127.0.0.1', () => {
 });
 */
 
-// ------------------------ Building a simple API ------------------------//
+// ----------------------------------- Building a simple API -----------------------------------//
 
 /*
 // Reading the file
@@ -113,7 +114,7 @@ server.listen(8000, '127.0.0.1', () => {
 */
 
 
-// ------------------------ Filling the templates ------------------------//
+// ----------------------------------- Filling the templates -----------------------------------//
 
 /*
 // Reading the Json file
@@ -123,7 +124,7 @@ const dataObj = JSON.parse(data);
 // Reading the files Seperately 
 const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
 const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
-const temProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
 
 // Creating a replaceTemplate Function that will return the 
 const replaceTemplate = (temp, product) => {
@@ -191,3 +192,91 @@ server.listen(8000, '127.0.0.1', () => {
      console.log('Listining to requests on port 8000');
 });
 */
+
+
+// ----------------------------------- Creating Product WebPages -----------------------------------//
+
+
+// Reading the Json file
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObj = JSON.parse(data);
+
+// Reading the files Seperately 
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
+
+// Creating a replaceTemplate Function that will return the 
+const replaceTemplate = (temp, product) => {
+
+     // Now replacing all the nicknames by the actual one by reading data from product.productDetails
+     let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+     output = output.replace(/{%IMAGE%}/g, product.image);
+     output = output.replace(/{%PRICE%}/g, product.price);
+     output = output.replace(/{%FROM%}/g, product.from);
+     output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+     output = output.replace(/{%QUANTITY%}/g, product.quantity);
+     output = output.replace(/{%DESCRIPTION%}/g, product.description);
+     output = output.replace(/{%ID%}/g, product.id);
+
+     // For replacing the organic part we first need to check
+     // If organic then change -->> Otherwise leave it likewise
+     if (!product.organic) {
+          output = output.replace(/{%NOT_ORGANIC%}/g, 'not-Organic');
+     }
+     // now returning the Replaced Html files
+     return output;
+}
+
+// Creating a server
+const server = http.createServer((req, res) => {
+
+     // Destructing the req.url to get the pathname and query(id of the product)
+     const {query, pathname} = url.parse(req.url, true);  // from the console.log coz of above code block 
+
+     // Responding as per server request -->> Overview webpage -->> 127.0.0.1:8000/overview
+     if (pathname === '/overview' || pathname === '/') {
+
+          // Telling the Node about the type of content
+          res.writeHead(200, {'content-type': 'text/html'});
+
+          // Replacing content of the template card
+          const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
+          const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+
+          // Showing the Content for the requested web page
+          res.end(output);
+
+     // Product Web page Loading -->> 127.0.0.1:8000/product?id=x
+     } else if (pathname === '/product') {
+          
+          // Telling the Node about the type of content
+          res.writeHead(200, {'content-type': 'text/html'});
+
+          // Extracting information about a specific id product from dataObj at line 202
+          const product = dataObj[query.id];
+          // Replacing the contents of the template-product.html
+          const output = replaceTemplate(tempProduct, product);
+          // showing the new Content
+          res.end(output);
+
+     // API Page -->> 127.0.0.1:8000/api
+     } else if (pathname === '/api') {
+          // Showing the data for requested url
+          res.writeHead(200, {'content-type': 'application/json'});
+          res.end(data);
+
+     // Not found Web Page -->> Invalid url
+     } else {
+          res.writeHead(404, {
+               'content-type': 'text/html',
+               'my-own-header': 'hello-world'
+          });
+          res.end("<h1>Page not found</h1>");
+     }
+});
+
+// Starting a server 
+server.listen(8000, '127.0.0.1', () => {
+     console.log('Listining to requests on port 8000');
+});

dd8441347b5a328b1912e4a58c8f577f18165d3f
Sourav Kumar <xsouravkumar357@gmail.com>
2025-10-07 06:51:38 +0530

    starter file code for node-farm

--- 2-how-node-works/modules.js
+++
@@ -1,9 +0,0 @@
-// -------------------------------- Modules --------------------------------//
-
-// console.log(arguments);
-// console.log(require("module").wrapper);
-
-// Importing the module (test-module-1.js)
-const C = require("./test-module-1");
-const cal1 = new C(); // Creating a new object of the C
-console.log(cal1.add(2, 5)); // Using the add method of the cal1 we just created

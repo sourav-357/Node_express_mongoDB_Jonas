49ff19c99d15bc82860898ce0283548a54658733
Sourav Kumar <xsouravkumar357@gmail.com>
2025-10-07 08:46:38 +0530

    async read and write filr

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -37,7 +37,28 @@ console.log('File Written Success...!');
 const fs = require('fs');
 
 // Reading datafrom another file -->> callback function -->> (err, data)
-fs.readFile('./txt/start.txt', 'utf-8', (err, data) => {
-    console.log(data);
+fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+
+    // Reading file 'read-this.txt' and storing data as data 2
+    fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+        console.log(data2);
+
+        // Reading another file with data as data3
+        fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
+            console.log(data3);
+
+            // writting the data2 and data3 in final.txt file 
+            fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', (err) => {
+                console.log('Data is written successfully...!🙂');
+
+                // reading the final data of final.txt
+                fs.readFile('./txt/final.txt', 'utf-8', (err, data) => {
+                    console.log(data);
+                })
+            });
+        });
+    });
 });
+
+// writing something to see if the async code works or not 
 console.log('Will read file now...');

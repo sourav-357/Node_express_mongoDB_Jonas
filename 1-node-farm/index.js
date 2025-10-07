a6073ad47183e62d38b671e3036b2b660e8ffcd8
Sourav Kumar <xsouravkumar357@gmail.com>
2025-10-07 08:22:59 +0530

    async read filr

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,6 +1,7 @@
 
 // ************************** Using Module 1 in Node.js **************************//
 
+/*
 // importing the fs module
 const fs = require('fs');
 
@@ -12,4 +13,30 @@ console.log(textIn);
 const textOut = `This is what we know about avocadro: ${textIn}\nCreated on ${Date.now()}`;
 fs.writeFileSync('./txt/final.txt', textOut);
 console.log('File Written Success...!');
+*/
 
+// ************************** Synchronous method (Blocking) **************************//
+
+/*
+// importing the fs module
+const fs = require('fs');
+
+// Reading data from another file
+const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
+console.log(textIn);
+
+// Writing the data on another file by the help of fs module
+const textOut = `This is what we know about avocadro: ${textIn}\nCreated on ${Date.now()}`;
+fs.writeFileSync('./txt/final.txt', textOut);
+console.log('File Written Success...!');
+*/
+
+// ************************** Asynchronous method (Non-Blocking) **************************//
+
+// importing the fs module
+const fs = require('fs');
+
+// Reading datafrom another file -->> callback function -->> (err, data)
+fs.readFile('./txt/start.txt', 'utf-8', (err, data) => {
+    console.log(data);
+});

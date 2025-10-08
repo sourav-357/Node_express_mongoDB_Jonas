66f34e488df2b73a43eea20a9a4f40939d69c5cf
Sourav Kumar <xsouravkumar357@gmail.com>
2025-10-08 13:13:29 +0530

    completed the streams file

--- 2-how-node-works/events.js
+++ 2-how-node-works/events.js
@@ -32,18 +32,21 @@ const server = http.createServer();
 server.on("request", (req, res) => {
      console.log("Listining to the server requests");
      res.end("Request recieved");
-})
+});
+
 // Again listining to the server with inbuilt emitter "request"
 server.on("request", (req, res) => {
      console.log("Listining to the server requests again");
-})
+});
+
 // Again listining to the server with inbuilt emitter "close"
 server.on("close", () => {
      console.log("Server Closed!");
-})
+});
+
 // Assign the server address 
 server.listen(8000, "127.0.0.1", () => {
      console.log("Waiting for requests ...");
-})
+});
 
 // -------------------------------- Created own Server --------------------------------//
--- 2-how-node-works/streams.js
+++ 2-how-node-works/streams.js
@@ -1,3 +1,5 @@
+
+// importing the required module
 const fs = require("fs");
 
 // Creating a server with less code and directly importing the https module
@@ -5,6 +7,7 @@ const server = require("http").createServer();
 
 // Starting the server now on request
 server.on("request", (req, res) => {
+
      //Solution 1 to read and write file
      // fs.readFile('test-file.txt', 'utf-8', (err, data) => { // Reading the data
      //      if (err) { // Handaling the error
@@ -14,20 +17,24 @@ server.on("request", (req, res) => {
      // })
 
      // Solution 2 for reading and writing the data
-     const readable = fs.createReadStream("test-file.txt"); // -->> This will read the file once its on
+     const readable = fs.createReadStream("./test-file.txt"); // -->> This will read the file once its on
+
+     // starting the readable
      readable.on("data", (chunk) => {
           // This will start to read the "data" in "chunks"
           res.write(chunk); // that chunk will start to get displayed
      });
+
      // when the entire content is read then we need to stop
      readable.on("end", () => {
           res.end();
      });
 });
+
 // server starting at the port
 server.listen(8000, "127.0.0.1", () => {
      // Listening the server
-     console.log("listining");
+     console.log("listining to server at port 8000 ...!");
 });
 
 // -------------------------------- Streams Completed--------------------------------//

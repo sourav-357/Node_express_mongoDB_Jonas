7612f1cae34302c3cd68c6e92547d995cb4a8064
Sourav Kumar <xsouravkumar357@gmail.com>
2025-10-09 11:44:44 +0530

    post method added

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -8,13 +8,20 @@ const app = express(); // Creating a server
 const port = 3000; // Creating a port number 
 
 
-// Routing the server 
+// Routing the server for '/get' method 
 app.get('/', (req, res) => {
-    res.status(200) // setting up the status code
+    res.status(404) // setting up the status code
     res.json({ // sending response to the browser
         message: 'Hellooo from the server side',
         app: 'Natours',
     });
+});
+
+
+// routing the server for '/' post method
+app.post('/', (req, res) => {
+    res.status(200); // status code for the request
+    res.send('Data sent success ....!'); // replying to the request 
 });
 
 

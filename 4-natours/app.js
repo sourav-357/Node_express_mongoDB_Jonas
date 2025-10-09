3febc88943698f290e8092beedbd3ef4d02c78c4
Sourav Kumar <xsouravkumar357@gmail.com>
2025-10-09 11:27:47 +0530

    installed nodemon

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -1,1 +1,25 @@
 
+// Importing the required modules
+const express = require('express');
+const http = require('http');
+
+// Creating a server
+const app = express();
+
+
+// Creating a port number 
+const port = 3000;
+
+
+// Routing the server 
+app.get('/', (req, res) => {
+    res.status(200)
+    res.send('Hellooo from the server side ....!')
+});
+
+
+// starting the server 
+app.listen(port, () => {
+    console.log(`App running on port ${port} ....!`)
+});
+
